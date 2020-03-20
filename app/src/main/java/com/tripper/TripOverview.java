0a0d657ce3c3dcdd933e1f18bc03b2961ce765ce
lynette <qgao38@wisc.edu>
2020-03-20 15:44:40 -0500

    set up the layout of the TripOverview page with bottom navigation view (hand-built)

---
+++ app/src/main/java/com/tripper/TripOverview.java
@@ -0,0 +1,52 @@
+package com.tripper;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+
+
+/* This class is the layout of the trip overview page with the bottomnavigation*/
+
+public class TripOverview extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_overview);
+
+        BottomNavigationView bottomNav = findViewById(R.id.tripoverview_bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListner);
+    }
+
+    //This method is to set up the bottom navigation function
+    private BottomNavigationView.OnNavigationItemSelectedListener navListner =
+            new BottomNavigationView.OnNavigationItemSelectedListener(){
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.nav_map:
+                            selectedFragment = new OverviewMapFragment();
+                            break;
+                        case R.id.nav_plan:
+                            selectedFragment = new OverviewPlanFragment();
+                            break;
+                        case R.id.nav_diary:
+                            selectedFragment = new OverviewDiaryFragment();
+                            break;
+
+
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+             };
+}
