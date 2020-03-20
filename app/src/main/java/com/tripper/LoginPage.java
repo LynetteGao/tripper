cd14cd8d90ba8f39c3e35324fdb7c5a11ae133e7
Mak <mak.yuwen@gmail.com>
2020-03-20 17:32:54 -0500

    Added splash screen, modified some stuff, linked homepage and login page!
    Note: in onCreate method, make sure to include setTheme(R.style.AppTheme)

--- app/src/main/java/com/tripper/LoginPage.java
+++ app/src/main/java/com/tripper/LoginPage.java
@@ -2,7 +2,9 @@ package com.tripper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -11,5 +13,9 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_AppCompat_DayNight_NoActionBar);
         setContentView(R.layout.activity_login_page);
+    }
+    public void logIn(View view) {
+        Intent intent = new Intent(LoginPage.this,HomePage.class);
+        startActivity(intent);
     }
 }
