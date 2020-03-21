86bb52792add6220e0f5beb54b15c1bbe9367dc5
Mak <mak.yuwen@gmail.com>
2020-03-21 13:17:37 -0500

    Fixed bugs

--- app/src/main/java/com/tripper/MainActivity.java
+++ app/src/main/java/com/tripper/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_AppCompat_Light_DarkActionBar);
         setContentView(R.layout.activity_main);
     }
 }
