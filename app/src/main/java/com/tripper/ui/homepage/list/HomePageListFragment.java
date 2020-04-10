2ee3d3454e930fb2ed47402c822ea7c962129566
haclark <haclark30@gmail.com>
2020-04-10 13:51:18 -0500

    made EmptyRecyclerView for displaying message when no trips created

--- app/src/main/java/com/tripper/ui/homepage/list/HomePageListFragment.java
+++ app/src/main/java/com/tripper/ui/homepage/list/HomePageListFragment.java
@@ -4,27 +4,24 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.tripper.R;
 import com.tripper.db.entities.Trip;
+import com.tripper.EmptyRecyclerView;
 
 import java.util.List;
 
 public class HomePageListFragment extends Fragment {
 
     private HomePageListViewModel homePageListViewModel;
-    private RecyclerView recyclerView;
+    private EmptyRecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
     private TripListAdapter adapter;
     private LiveData<List<Trip>> trips;
@@ -43,9 +40,14 @@ public class HomePageListFragment extends Fragment {
 
         trips = homePageListViewModel.getTrips();
         adapter = new TripListAdapter(getContext());
-        trips.observe(getViewLifecycleOwner(), trips -> adapter.setData(trips));
-        recyclerView = root.findViewById(R.id.tripRecyclerView);
+
+
+        recyclerView = (EmptyRecyclerView) root.findViewById(R.id.tripRecyclerView);
         recyclerView.setLayoutManager(layoutManager);
+
+        View emptyView = root.findViewById(R.id.txtNoTrips);
+        recyclerView.setEmptyView(emptyView);
+        trips.observe(getViewLifecycleOwner(), trips -> adapter.setData(trips));
         recyclerView.setAdapter(adapter);
 
         return root;
