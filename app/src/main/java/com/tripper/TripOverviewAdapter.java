9fea6e71adceea920285a2ded04befc620dc6b1f
lynette <qgao38@wisc.edu>
2020-04-28 21:08:13 -0500

    set up the overview page interaction with db

--- app/src/main/java/com/tripper/TripOverviewAdapter.java
+++ app/src/main/java/com/tripper/TripOverviewAdapter.java
@@ -1,6 +1,9 @@
 package com.tripper;
 
 import android.content.Context;
+import android.location.Address;
+import android.location.Geocoder;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,22 +14,28 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.tripper.db.entities.Day;
 import com.tripper.db.entities.Trip;
+import com.tripper.db.relationships.DaySegmentWithEvents;
 import com.tripper.db.relationships.DayWithSegmentsAndEvents;
 import com.tripper.db.relationships.TripWithDaysAndDaySegments;
 import com.tripper.ui.homepage.list.HomePageListViewModel;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+
+import static android.content.ContentValues.TAG;
 
 public class TripOverviewAdapter extends RecyclerView.Adapter<TripOverviewAdapter.TripHolder> {
 
     //private List<DayWithSegmentsAndEvents> day_item;
-
+    Geocoder geocoder;
     private List<DayWithSegmentsAndEvents> day_item;
     private Context context;
     //private LayoutInflater layoutInflater;
     //private HomePageListViewModel homePageListViewModel;
     private TripOverviewViewModel tripOverviewViewModel;
+    List<Address> addresses;
 
 
     public TripOverviewAdapter(Context context, TripOverviewViewModel tripOverviewViewModel, TripWithDaysAndDaySegments trip_with_days) {
@@ -34,6 +43,7 @@ public class TripOverviewAdapter extends RecyclerView.Adapter<TripOverviewAdapte
         //this.layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.tripOverviewViewModel = tripOverviewViewModel;
         this.day_item = trip_with_days.days;
+        geocoder = new Geocoder(context, Locale.getDefault());
     }
 
 
@@ -48,16 +58,46 @@ public class TripOverviewAdapter extends RecyclerView.Adapter<TripOverviewAdapte
     @Override
     public void onBindViewHolder(@NonNull TripHolder holder, int position) {
         Day currentDay = day_item.get(position).day;
-        //Trip currentTrip = day_item.get(position);//TODO
-        String date = String.format("%1$tb %1$te, %1$tY", currentDay.date);
 
         int dayCount  = position+1;
-        holder.textViewAddress.setText("Day "+ dayCount);
+        String date = String.format("%1$tb %1$te, %1$tY", currentDay.date);
+        holder.textViewDay.setText("Day "+ dayCount);
 
-        holder.textViewDay.setText(date);//TODO
-        holder.textViewMorning.setText("Morning");
+
+//        Double lat = Double.valueOf(currentDay.locationLat);
+//        Double lon = Double.valueOf(currentDay.locationLon);
+//        Log.d(TAG, "onBindViewHolder: "+ lat);
+//        try {
+//            addresses = geocoder.getFromLocation(lat, lon, 1);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        String address = addresses.get(0).getAddressLine(0);
+        holder.textViewAddress.setText(date);
+
+        holder.textViewMorning.setText("Morning:" );
         holder.textViewAfternoon.setText("Afternoon");
         holder.textViewEvening.setText("Evening");
+
+        List<DaySegmentWithEvents> segmentsAndEvents = day_item.get(position).daySegments;
+        Log.d(TAG, "onBindViewHolder: ");
+        if( segmentsAndEvents!=null& segmentsAndEvents.size()>=1 && segmentsAndEvents.get(0).events.size()>=1 ) {
+            String mEvent = segmentsAndEvents.get(0).events.get(0).name;
+            holder.textViewMorning.setText("Morning: " + mEvent);
+        }
+        if(segmentsAndEvents!=null& segmentsAndEvents.size()>=2 && segmentsAndEvents.get(1).events.size()>=1) {
+            String aEvent = segmentsAndEvents.get(1).events.get(0).name;
+            holder.textViewMorning.setText("Afternoon: " + aEvent);
+        }
+        if(segmentsAndEvents!=null& segmentsAndEvents.size()>=3 && segmentsAndEvents.get(2).events.size()>=1) {
+            String eEvent = segmentsAndEvents.get(2).events.get(0).name;
+            holder.textViewMorning.setText("Evening: " + eEvent);
+        }
+
+
+        //holder.textViewMorning.setText("Morning:" + mEvent);
+
+
     }
 
     @Override
