55f5bf7d53e7ec1b834f2cfbb644d50e5d06ab56
haclark30 <hjclark@wisc.edu>
2020-05-06 09:28:02 -0500

    created menu for trip overview with button to get back to homepage

--- app/src/main/java/com/tripper/TripOverview.java
+++ app/src/main/java/com/tripper/TripOverview.java
@@ -4,7 +4,10 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -14,6 +17,24 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 /* This class is the layout of the trip overview page with the bottomnavigation*/
 
 public class TripOverview extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.tripoverview_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.trip_overview_home_button:
+                Intent intent = new Intent(this, HomePage.class);
+                this.startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(menuItem);
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
